9198d03ff9681512c9461b12257ab74a7af4f5e4
chinapat <chinapat.r@ku.th>
2024-07-30 12:27:19 +0700

    add xml parser function into app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+//Module
 const express = require('express');
+const convert = require('./xml_convert')
+const xmlparser = require('express-xml-bodyparser');
 const app = express ();
+
+//Application setting
 app.use(express.json());
+app.use(xmlparser({explicitArray:false}));
+
 const PORT = process.env.PORT || 3000;
+
+//API
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
   });
@@ -9,6 +18,8 @@ app.get("/status", (request, response) => {
    const status = {
       "Status": "Running"
    };
-   
    response.send(status);
+});
+app.post("/upload", function(request, response){
+    response.send(request.body)
 });
